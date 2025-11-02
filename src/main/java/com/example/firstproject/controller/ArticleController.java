ce5f575a9edb6745ee2cb780b436e4fce1488d88
zi-eunn <jieunkim1117@gmail.com>
2025-11-02 20:24:35 +0900

    feat: DTO DB에 저장

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,10 +1,13 @@
 package com.example.firstproject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import com.example.firstproject.repository.ArticleRepository;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
 
 @Controller
 public class ArticleController {
@@ -13,9 +16,20 @@ public class ArticleController {
 		return "articles/new";
 	}
 
+	@Autowired
+	private ArticleRepository articleRepository;
+
 	@PostMapping("/articles/create") // post 형식으로 정보를 전달 받았으니까 post임. 보통 로그인 등 중요한 정보를 담음
 	public String createArticle(ArticleForm form) { // 폼 데이터를 dto로 받기
 		System.out.println(form.toString()); // dto에 폼 데이터가 잘 담겼는지 확인
+
+		//1. DTO를 엔티티로 변환
+		Article article = form.toEntity(); //form 객체의 toEntity라는 메서드를 호출해서 반환값을 Article 타입의 article 엔티티에 저장
+		System.out.println(article.toString());
+		//2. 리파지터리로 엔티티를 DB에 저장
+		Article saved = articleRepository.save(article); //article 엔티티를 저장해 saved 객체에 반환
+		System.out.println(saved.toString());
 		return "";
 	}
 }
+-
---
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -0,0 +1,8 @@
+package com.example.firstproject.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.firstproject.entity.Article;
+
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+}
